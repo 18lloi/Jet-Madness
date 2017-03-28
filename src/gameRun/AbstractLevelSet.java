4bc0433982fda5c4aed16e8121adefe891d7a17d
alexl_000 <alex.liu975@gmail.com>
2017-03-27 21:04:09 -0400

    trash

--- src/gameRun/AbstractLevelSet.java
+++ src/gameRun/AbstractLevelSet.java
@@ -16,5 +16,7 @@ public abstract class AbstractLevelSet {
 	public abstract void keyPressed(int k); // user input
 
 	public abstract void keyReleased(int k); // stops user input
+	
+	print("dkfa;dk;")
 
 }
