31cce25ac1302e3cd43670328bbc42c30a1d6b29
alexl_000 <alex.liu975@gmail.com>
2017-03-27 21:32:08 -0400

    a

--- src/gameRun/AbstractLevelSet.java
+++ src/gameRun/AbstractLevelSet.java
@@ -18,7 +18,5 @@ public abstract class AbstractLevelSet {
 	public abstract void keyPressed(int k); // user input
 
 	public abstract void keyReleased(int k); // stops user input
-	
-kDHGkUSEGslHglSUEg
 
 }
